dbcb79443fd6943741454d685023785a349024ac
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2021-11-20 14:38:51 +0530

    create LCM.c

---
+++ LCM.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int lcm(int a,int b);
+int main()
+{ int n,i,j;
+printf("Input first nember ",n);
+scanf("%d",&n);
+printf("input second number ",i);
+scanf("%d",&i);
+{printf("LCM of %d and %d : %d",n,i,lcm(n,i));}
+return 0;
+}
+int lcm(n,i)
+{ int j,a;
+ j=j+a;
+ if(j%n==0 && j%i==0)
+ return j;
+ return(n,i);
+	
+}
